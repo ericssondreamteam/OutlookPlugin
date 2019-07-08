ab329f8f60c2f286b9028e919b31aa6544e49f40
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-07-08 13:13:52 +0200

    usunięte niepotrzbene foldery... chyba będzie działać

--- Ribbon1.cs
+++ Ribbon1.cs
@@ -108,7 +108,7 @@ namespace OutlookAddIn1
                        
 
                     }
-                    workSheet.SaveAs(value,"docx","haslo","sao",false);
+                    workSheet.SaveAs(value);
                     MessageBox.Show("Your raport is saved in: " + value);
                     //MessageBox.Show(c);
                 }

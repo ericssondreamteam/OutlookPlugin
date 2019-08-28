c548d7d397b779887e284609c8e119b92f93ee0b
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-08-28 08:38:40 +0200

    polski to english

--- Loading.Designer.cs
+++ Loading.Designer.cs
@@ -35,19 +35,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(192, 158);
+            this.label1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label1.Location = new System.Drawing.Point(177, 154);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(380, 50);
+            this.label1.Size = new System.Drawing.Size(453, 50);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Twój raport się tworzy ...";
+            this.label1.Text = "Your report is being created ...";
             this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
             // Loading
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
             this.ClientSize = new System.Drawing.Size(767, 377);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
